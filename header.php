3f838f792c36ae13a38999678983d76302cbf679
yyooppee <155448544+yyooppee@users.noreply.github.com>
2024-01-15 23:05:51 +0800

    image

--- header.php
+++ header.php
@@ -1,7 +1,10 @@
 <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
       <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
         <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+        <div>
+        <img src="pics/mimis_logo.jpg" width="45" height="45" alt="Logo">
         <span class="fs-4">Mimi's Pet Shop</span>
+        </div>
       </a>
 
       <ul class="nav nav-pills">
