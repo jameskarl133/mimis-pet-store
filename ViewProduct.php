f06c9cb51aadf92741b6a0945224d0d65b423acd
jameskarl133 <155448698+jameskarl133@users.noreply.github.com>
2024-01-17 09:46:48 +0800

    .

--- ViewProduct.php
+++ ViewProduct.php
@@ -3,7 +3,7 @@ include "components/db.php";
 
 // Function to get all products along with their inventory status
 function getAllProductsWithInventoryStatus($conn, $search = "") {
-    $query = "SELECT product.*, inventory.inv_item_status FROM product
+    $query = "SELECT product.*, inventory.inv_item_status, inventory.inv_item_qty FROM product
               LEFT JOIN inventory ON product.prod_id = inventory.prod_id";
 
     // Add search condition if provided
@@ -51,6 +51,7 @@ $productResultWithStatus = getAllProductsWithInventoryStatus($conn, $searchTerm)
                     <th>Product Description</th>
                     <th>Product Price</th>
                     <th>Brand</th>
+                    <th>Quantity</th>
                     <th>Inventory Status</th>
                 </tr>
                 <?php
@@ -62,6 +63,7 @@ $productResultWithStatus = getAllProductsWithInventoryStatus($conn, $searchTerm)
                         <td><?php echo $row['prod_desc']; ?></td>
                         <td><?php echo $row['prod_price']; ?></td>
                         <td><?php echo $row['prod_brand']; ?></td>
+                        <td><?php echo isset($row['inv_item_qty']) ? $row['inv_item_qty'] : 'N/A'; ?></td>
                         <td><?php echo isset($row['inv_item_status']) ? $row['inv_item_status'] : 'N/A'; ?></td>
                     </tr>
                     <?php
@@ -70,29 +72,30 @@ $productResultWithStatus = getAllProductsWithInventoryStatus($conn, $searchTerm)
             </table>
         </div>
     </div>
+
     <script>
-    function searchProducts() {
-        let input, filter, table, tr, td, i, txtValue;
-        input = document.getElementById("search");
-        filter = input.value.toUpperCase();
-        table = document.querySelector("table");
-        tr = table.getElementsByTagName("tr");
+        function searchProducts() {
+            let input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById("search");
+            filter = input.value.toUpperCase();
+            table = document.querySelector("table");
+            tr = table.getElementsByTagName("tr");
 
-        for (i = 0; i < tr.length; i++) {
-            // Skip the header row
-            if (i === 0) continue;
+            for (i = 0; i < tr.length; i++) {
+                // Skip the header row
+                if (i === 0) continue;
 
-            let found = false;
-            for (td of tr[i].getElementsByTagName("td")) {
-                txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                    found = true;
-                    break;
+                let found = false;
+                for (td of tr[i].getElementsByTagName("td")) {
+                    txtValue = td.textContent || td.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        found = true;
+                        break;
+                    }
                 }
+                tr[i].style.display = found ? "" : "none";
             }
-            tr[i].style.display = found ? "" : "none";
         }
-    }
-</script>
+    </script>
 </body>
 </html>
